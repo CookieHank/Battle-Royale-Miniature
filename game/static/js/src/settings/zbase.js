56a78ae22d3d0bd12f9622fca5eefdc1578ebeb3
Yucheng <hankgycc@gmail.com>
2024-01-25 16:26:50 -0500

    Login Update

--- game/static/js/src/settings/zbase.js
+++ game/static/js/src/settings/zbase.js
@@ -149,7 +149,7 @@ class Settings {
 
     acwing_login() {
         $.ajax({
-            url: "https://app165.acapp.acwing.com.cn/settings/acwing/web/apply_code/",
+            url: "https://app5736.acapp.acwing.com.cn/settings/acwing/web/apply_code/",
             type: "GET",
             success: function (resp) {
                 if (resp.result === "success") {
@@ -166,7 +166,7 @@ class Settings {
         this.$login_error_message.empty();
 
         $.ajax({
-            url: "https://app165.acapp.acwing.com.cn/settings/login/",
+            url: "https://app5736.acapp.acwing.com.cn/settings/login/",
             type: "GET",
             data: {
                 username: username,
@@ -190,7 +190,7 @@ class Settings {
         this.$register_error_message.empty();
 
         $.ajax({
-            url: "https://app165.acapp.acwing.com.cn/settings/register/",
+            url: "https://app5736.acapp.acwing.com.cn/settings/register/",
             type: "GET",
             data: {
                 username: username,
@@ -212,7 +212,7 @@ class Settings {
             this.root.AcWingOS.api.window.close();
         } else {
             $.ajax({
-                url: "https://app165.acapp.acwing.com.cn/settings/logout/",
+                url: "https://app5736.acapp.acwing.com.cn/settings/logout/",
                 type: "GET",
                 success: function (resp) {
                     if (resp.result === "success") {
@@ -250,7 +250,7 @@ class Settings {
         let outer = this;
 
         $.ajax({
-            url: "https://app165.acapp.acwing.com.cn/settings/acwing/acapp/apply_code/",
+            url: "https://app5736.acapp.acwing.com.cn/settings/acwing/acapp/apply_code/",
             type: "GET",
             success: function (resp) {
                 if (resp.result === "success") {
@@ -264,7 +264,7 @@ class Settings {
         let outer = this;
 
         $.ajax({
-            url: "https://app165.acapp.acwing.com.cn/settings/getinfo/",
+            url: "https://app5736.acapp.acwing.com.cn/settings/getinfo/",
             type: "GET",
             data: {
                 platform: outer.platform,
