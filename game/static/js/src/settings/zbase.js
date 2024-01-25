9924ef1ac482dd6a34bd8b90c45e42bce5d572a9
Yucheng <hankgycc@gmail.com>
2024-01-25 14:27:12 -0500

    Login and Register Update

--- game/static/js/src/settings/zbase.js
+++ game/static/js/src/settings/zbase.js
@@ -2,64 +2,148 @@ class Settings {
     constructor(root) {
         this.root = root;
         this.platform = "WEB";
-        if (this.root.AcWingOS) this.platform = 'ACAPP';
-        this.username = '';
-        this.photo = '';
+        if (this.root.AcWingOS) this.platform = "ACAPP";
+        this.username = "";
+        this.photo = "";
 
         this.$settings = $(`
 <div class="ac-game-settings">
     <div class="ac-game-settings-login">
         <div class="ac-game-settings-title">
-            Sign In
+            Login
         </div>
         <div class="ac-game-settings-username">
             <div class="ac-game-settings-item">
                 <input type="text" placeholder="Username">
             </div>
         </div>
-        <div>
-            <div class="ac-game-settings-password">
-                <div class="ac-game-settings-item">
-                    <input type="password" placeholder="Password">
-                </div>
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password">
             </div>
-            <div class="ac-game-settings-submit">
-                <div class="ac-game-settings-item">
-                    <button>Sign In</button>
-                </div>
-            </div>
-            <div class="ac-game-settings-error-message"></div>
-            <div class="ac-game-settings-option">Register<div>
-            <div class="ac-game-settings-acwing">
-                <img width="30" src="https://app5736.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
-                <br>
-            </div>
-
         </div>
-
-
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>Login</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-error-message">
+        </div>
+        <div class="ac-game-settings-option">
+            Register
+        </div>
+        <br>
+        <div class="ac-game-settings-acwing">
+            <img width="30" src="https://app165.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+            <br>
+            <div>
+                AcWing Third Party Login
+            </div>
+        </div>
     </div>
-
-    <div class="ad-game-settings-register">
-
+    <div class="ac-game-settings-register">
+        <div class="ac-game-settings-title">
+            Register
+        </div>
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="Username">
+            </div>
+        </div>
+        <div class="ac-game-settings-password ac-game-settings-password-first">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password">
+            </div>
+        </div>
+        <div class="ac-game-settings-password ac-game-settings-password-second">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password Confirm">
+            </div>
+        </div>
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>Register</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-error-message">
+        </div>
+        <div class="ac-game-settings-option">
+            Login
+        </div>
+        <br>
+        <div class="ac-game-settings-acwing">
+            <img width="30" src="https://app165.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+            <br>
+            <div>
+                AcWing Third Party Login
+            </div>
+        </div>
     </div>
-</div>            
-    `);
-
+</div>
+`);
         this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login_username = this.$login.find(".ac-game-settings-username input");
+        this.$login_password = this.$login.find(".ac-game-settings-password input");
+        this.$login_submit = this.$login.find(".ac-game-settings-submit button");
+        this.$login_error_message = this.$login.find(".ac-game-settings-error-message");
+        this.$login_register = this.$login.find(".ac-game-settings-option");
+
         this.$login.hide();
 
         this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register_username = this.$register.find(".ac-game-settings-username input");
+        this.$register_password = this.$register.find(".ac-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".ac-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".ac-game-settings-submit button");
+        this.$register_error_message = this.$register.find(".ac-game-settings-error-message");
+        this.$register_login = this.$register.find(".ac-game-settings-option");
+
         this.$register.hide();
+
+        this.$acwing_login = this.$settings.find('.ac-game-settings-acwing img');
 
         this.root.$ac_game.append(this.$settings);
 
         this.start();
     }
 
+
     start() {
         this.getinfo();
+        this.add_listening_events();
     }
+
+    add_listening_events() {
+        let outer = this;
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+
+        this.$acwing_login.click(function () {
+            outer.acwing_login();
+        });
+    }
+
+    add_listening_events_login() {
+        let outer = this;
+
+        this.$login_register.click(function () {
+            outer.register();
+        });
+        // this.$login_submit.click(function () {
+        //     outer.login_on_remote();
+        // });
+    }
+
+    add_listening_events_register() {
+        let outer = this;
+        this.$register_login.click(function () {
+            outer.login();
+        });
+        // this.$register_submit.click(function () {
+        //     outer.register_on_remote();
+        // });
+    }
+
 
     register() { // open the register page
         this.$login.hide();
